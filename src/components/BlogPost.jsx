4c550c3d09bcadd4d1a28e719d8625cdbacdd7f7
UP2019511 <savvikopikovs@yahoo.com>
2025-03-11 17:22:07 +0000

    scores

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -4,7 +4,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 
-const BlogPost = ({ title, content, photos }) => {
+const BlogPost = ({ title, content, photos, r1, r2, r1name, r2name }) => {
   const settings = {
     dots: true,
     infinite: false,
@@ -28,6 +28,16 @@ const BlogPost = ({ title, content, photos }) => {
         </div>
         <h2>{title}</h2>
         <p>{content}</p>
+        <div className="ratingBox">
+          <div className='button two'>
+            <p>{r1name}</p>
+            <p className='bolder'>{r1}</p>
+          </div>
+          <div className='button two'>
+            <p>{r2name}</p>
+            <p className='bolder'>{r2}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
